612a5b326f8c29e684bd91040a920f5ab5958d1f
Jadon Procter <procter.jadon@gmail.com>
2022-05-06 15:08:52 +0100

    added fflush() to all required instances.

--- smallsh.c
+++ smallsh.c
@@ -85,6 +85,7 @@ void expand(char *s, pid_t p)
     tmp[strlen(tmp) - 1] = '\0'; // null terminate the string
     strcpy(s, tmp);              // overwrite s with the expanded array
     printf("%s", s);
+    fflush(stdout);
     // free memory used on the heap.
     free(dollarArray);
     free(tmp);
@@ -120,11 +121,12 @@ int main()
         int status = 0; // status save
 
         // 1a. USE THE COLON AS A PROMPT FOR EACH LINE
-        fflush(stdout); // flush to sanatize output
-        printf(":");
 
-        fflush(stdin);                                             // flush to sanatize input
+        printf(":");
+        fflush(stdout); // flush to sanatize output
+
         size_t bytesRead = getline(&command, &commandSize, stdin); // get entire line.
+        fflush(stdin);                                             // flush to sanatize inputs
 
         // remove new line
         command[bytesRead - 1] = '\0';
@@ -201,55 +203,61 @@ int main()
                 if (cdStat < 0)
                 {
                     printf("Error: %s is not a directory", args[0]);
+                    fflush(stdout);
                 }
                 getcwd(dir, 100);
                 printf("After: %s\n", dir);
+                fflush(stdout);
             }
             else
             {
                 char dir[100];
                 getcwd(dir, 100);
                 printf("Before: %s\n", dir);
+                fflush(stdout);
                 chdir(getenv("HOME"));
                 getcwd(dir, 100);
                 printf("After: %s\n", dir);
+                fflush(stdout);
             }
         }
         // STATUS. - prints the exit status or the last foreground process status. (ignore 3 built in commands).
         if (strcmp(cmd, "status") == 0)
         {
             printf("STATUS is: %d\n", status);
+            fflush(stdout);
         }
+
+        // 5. EXECUTING OTHER COMMANDS.
+        // When non-built-in command fork() a child process. the child will then use exec().
+        // Your shell should use PATH and allow shell scripts to be exectuted.
+        // If no command found then exit status 1.
+        // terminate child process
+
+        // 6. INPUT AND OUTPUT REDIRECTION.
+        // Input redirected on stdin only open for reading. Print error and status 1 smallsh if unable.
+        // Output redirected on stdout only open for writing. Print error and status 1 smallsh if unable.
+        // Both can be redirected at the same time.
+
+        // 7. FOREGROUND AND BACKGROUND COMMANDS.
+        // Forground doesn't return foreground access until termination.
+        // Background prints pid and on termination prints pid and status.
+        // If no input redirect on a background command redirect will be /dev/null.
+        // If no output redirect on a background command redirect will be /dev/null.
+
+        // 8. SIGNALS: SIGINT & SIGTSTP.
+        // SIGINT:
+        // Ctrl-C will send SIGINT to parent process and all children at the same time.
+        // Children terminate self in FG and if in BG process ignore SIGINT.
+        // Parent FG process will print out signal.
+        // SIGTSTP:
+        // FG child processes ignore.
+        // BG child processes ignore.
+        // Parent must display an informative message.
+        // Shell will then no longer run background processes.
+        // '&' will be ignored.
+        // A second SIGTSTP will return the shell to normal.
     }
 
-    // 5. EXECUTING OTHER COMMANDS.
-    // When non-built-in command fork() a child process. the child will then use exec().
-    // Your shell should use PATH and allow shell scripts to be exectuted.
-    // If no command found then exit status 1.
-    // terminate child process
-
-    // 6. INPUT AND OUTPUT REDIRECTION.
-    // Input redirected on stdin only open for reading. Print error and status 1 smallsh if unable.
-    // Output redirected on stdout only open for writing. Print error and status 1 smallsh if unable.
-    // Both can be redirected at the same time.
-
-    // 7. FOREGROUND AND BACKGROUND COMMANDS.
-    // Forground doesn't return foreground access until termination.
-    // Background prints pid and on termination prints pid and status.
-    // If no input redirect on a background command redirect will be /dev/null.
-    // If no output redirect on a background command redirect will be /dev/null.
-
-    // 8. SIGNALS: SIGINT & SIGTSTP.
-    // SIGINT:
-    // Ctrl-C will send SIGINT to parent process and all children at the same time.
-    // Children terminate self in FG and if in BG process ignore SIGINT.
-    // Parent FG process will print out signal.
-    // SIGTSTP:
-    // FG child processes ignore.
-    // BG child processes ignore.
-    // Parent must display an informative message.
-    // Shell will then no longer run background processes.
-    // '&' will be ignored.
-    // A second SIGTSTP will return the shell to normal.
     return 0;
 }
